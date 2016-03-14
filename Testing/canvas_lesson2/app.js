f213db52605ffb9afdf14492632e6948187e1e14
kilowatt11 <kilowatt11@cableone.net>
2016-03-13 20:40:52 -0600

    changed x ,y  variables to randomly generate on the x axis and float up on the y axis instead of across the x

--- Testing/canvas_lesson2/app.js
+++ Testing/canvas_lesson2/app.js
@@ -3,13 +3,13 @@ function draw(x,y){
     var ctx =canvas.getContext("2d");
     ctx.save(); //save canvas state if required
     ctx.clearRect(0,0,550,400); //clear the canvas for redrawing
-    ctx.fillStyle = "rgba(0,200,0,1)"; //styel for a green rectangle
-    ctx.fillRect (x, 20, 50, 50); //draw the rectangle
-        //x is dynamic and can change to give the impression of animation
-        //20 is the y paramiter
-        //50,50 is the height and widht of the 
+    ctx.fillStyle = "rgba(0,200,0,1)"; //style for a green rectangle
+    ctx.fillRect (x, y, 50, 50); //draw the rectangle
+        //x and y are dynamic and can change to give the impression of animation
+        
+        //50,50 is the height and width of the rectangle
         ctx.restore(); // restore canvas state if saved
-        x += 1;  //increment the x position by numerical value
+        y -= 1;  //decrement, the y position by numerical value
         var loopTimer = setTimeout('draw('+x+', '+y+')',10);
     
 }
