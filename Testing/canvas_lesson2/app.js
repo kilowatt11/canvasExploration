e244d0f9086bb07dc7add6488dd87b2b9a8a636e
kilowatt11 <kilowatt11@cableone.net>
2016-03-14 18:13:44 -0600

    changed rectangle to circle

--- Testing/canvas_lesson2/app.js
+++ Testing/canvas_lesson2/app.js
@@ -1,17 +1,33 @@
-function draw(x,y){
+function circle(x, y, r) {
+    //allow us to start a new path
+    ctx.beginPath();
+    //to define the size and shape of the circle
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    //fill the circle with default color (black)
+    ctx.fill();
+};
+
+function draw(x, y) {
     var canvas = document.getElementById('canvas');
-    var ctx =canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
     ctx.save(); //save canvas state if required
-    ctx.clearRect(0,0,550,400); //clear the canvas for redrawing
-    ctx.fillStyle = "rgba(0,200,0,1)"; //style for a green rectangle
-    ctx.fillRect (x, y, 50, 50); //draw the rectangle
-        //x and y are dynamic and can change to give the impression of animation
-        
-        //50,50 is the height and width of the rectangle
-        ctx.restore(); // restore canvas state if saved
-        y -= 1;  //decrement, the y position by numerical value
-        var loopTimer = setTimeout('draw('+x+', '+y+')',10);
-    
-    
+    ctx.clearRect(0, 0, 550, 400); //clear the canvas for redrawing
+    //ctx.fillStyle = "rgba(0,200,0,1)"; //style for a green rectangle
+    function circle(x, y, r) {
+        //allow us to start a new path
+        ctx.beginPath();
+        //to define the size and shape of the circle
+        ctx.arc(x, y, r, 0, Math.PI * 2, true);
+        //fill the circle with default color (black)
+        ctx.fill();
+    }; circle(x, y, 20) //draw the rectangle
+    //x and y are dynamic and can change to give the impression of animation
+
+    //50,50 is the height and width of the rectangle
+    ctx.restore(); // restore canvas state if saved
+    y -= 1;  //decrement, the y position by numerical value
+    var loopTimer = setTimeout('draw(' + x + ', ' + y + ')', 10);
+
+
     //can create multiple boxes on canvas, but unsure of how to fix flicker currently. can change these to circles for bubbles
 }
